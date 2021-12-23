219af05af581cafffbd8150ed9fde0fa31d5d899
Alice Boxhall <aboxhall@chromium.org>
2021-12-24 09:40:00 +1100

    cleanup day 3

--- src/day_03.rs
+++ src/day_03.rs
@@ -2,6 +2,78 @@ use std::io;
 
 pub fn run() {
     let mut diagnostic = Vec::<u32>::new();
+    let size = read_diagnostic_from_file(&mut diagnostic);
+
+    let mut pos = size - 1;
+    let mut potential_oxygen_ratings = diagnostic.to_vec();
+    let mask = 1 << pos;
+    let filter = most_common_bit(pos, &potential_oxygen_ratings) << pos;
+    let mut potential_carbon_ratings: Vec<u32> = potential_oxygen_ratings
+        .drain_filter(|x| *x & mask != filter)
+        .collect();
+
+    println!(
+        "after draining: potential_carbon_ratings {:?}, potential_oxygen_ratings: {:?}",
+        potential_carbon_ratings, potential_oxygen_ratings
+    );
+
+    let oxygen_rating = find_rating(&mut potential_oxygen_ratings, pos, true);
+    let carbon_rating = find_rating(&mut potential_carbon_ratings, pos, false);
+
+    println!(
+        "oxygen_rating: {:?}, carbon_rating: {:?}, product: {}",
+        oxygen_rating,
+        carbon_rating,
+        oxygen_rating * carbon_rating
+    );
+}
+
+fn find_rating(potential_ratings: &mut Vec<u32>, start_pos: usize, most: bool) -> u32 {
+    let mut pos = start_pos;
+    loop {
+        pos -= 1;
+        let mut count = 0;
+        let mask = 1 << pos;
+        let most_common = most_common_bit(pos, &potential_ratings);
+        let bit = if most { most_common } else { most_common ^ 1 };
+        let filter = bit << pos;
+        println!(
+            "pos: {}, most_common: {:b}, bit: {:b}, mask: {:b}, filter: {:b}",
+            pos, most_common, bit, mask, filter
+        );
+
+        let _ = potential_ratings
+            .drain_filter(|x| *x & mask != filter)
+            .count();
+        println!("after draining: potential_ratings {:?}", potential_ratings);
+
+        if potential_ratings.len() == 1 {
+            return potential_ratings[0];
+        }
+    }
+    panic!("No rating found");
+}
+
+fn most_common_bit(pos: usize, diagnostic: &Vec<u32>) -> u32 {
+    let mut count = 0;
+    let mask = 1 << pos;
+    for binary in diagnostic {
+        let bit = (*binary & mask) / mask;
+        match bit {
+            0 => {
+                count -= 1;
+            }
+            1 => {
+                count += 1;
+            }
+            _ => {}
+        }
+    }
+
+    return if count >= 0 { 1 } else { 0 };
+}
+
+fn read_diagnostic_from_file(diagnostic: &mut Vec<u32>) -> usize {
     let mut size: usize = 0;
 
     loop {
@@ -25,73 +97,5 @@ pub fn run() {
         }
     }
 
-    let mut potential_oxygen_ratings = diagnostic.to_vec();
-
-    let mut pos = size;
-    loop {
-        pos -= 1;
-        let mut count = 0;
-        let mask = 1 << pos;
-        for binary in &potential_oxygen_ratings {
-            let bit = (*binary & mask) / mask;
-            match bit {
-                0 => {
-                    count -= 1;
-                }
-                1 => {
-                    count += 1;
-                }
-                _ => {}
-            }
-        }
-
-        let filter = (if count >= 0 { 1 } else { 0 }) << pos;
-        let _ = potential_oxygen_ratings
-            .drain_filter(|x| *x & mask != filter)
-            .count();
-        println!("after draining: {:?}", potential_oxygen_ratings);
-
-        if potential_oxygen_ratings.len() == 1 {
-            break;
-        }
-    }
-    let oxygen_rating = potential_oxygen_ratings[0];
-
-    let mut potential_carbon_ratings = diagnostic.to_vec();
-    let mut pos = size;
-    loop {
-        pos -= 1;
-        let mut count = 0;
-        let mask = 1 << pos;
-        for binary in &potential_carbon_ratings {
-            let bit = (*binary & mask) / mask;
-            match bit {
-                1 => {
-                    count -= 1;
-                }
-                0 => {
-                    count += 1;
-                }
-                _ => {}
-            }
-        }
-
-        let filter = (if count > 0 { 1 } else { 0 }) << pos;
-        let _ = potential_carbon_ratings
-            .drain_filter(|x| *x & mask != filter)
-            .count();
-        println!("after draining: {:?}", potential_carbon_ratings);
-
-        if potential_carbon_ratings.len() == 1 {
-            break;
-        }
-    }
-    let carbon_rating = potential_carbon_ratings[0];
-
-    println!(
-        "oxygen_rating: {:?}, carbon_rating: {:?}, product: {}",
-        oxygen_rating,
-        carbon_rating,
-        oxygen_rating * carbon_rating
-    );
+    return size;
 }
